86e9a467808cd7138c17757697e8e6ab592e14d3
Satanya <admin@sozr.cn>
2020-01-17 13:53:15 +0800

    aa

--- src/main/kotlin/GeoMain.kt
+++ src/main/kotlin/GeoMain.kt
@@ -1,11 +1,13 @@
-import managers.CanManager.canCollection
 import com.mongodb.MongoClientSettings
 import com.mongodb.MongoException
 import com.mongodb.client.MongoClients
 import com.mongodb.client.MongoDatabase
 import handlers.handleCommand
 import handlers.registerListeners
-import hazae41.minecraft.kutils.bukkit.*
+import hazae41.minecraft.kutils.bukkit.BukkitPlugin
+import hazae41.minecraft.kutils.bukkit.info
+import hazae41.minecraft.kutils.bukkit.warning
+import managers.CanManager.canCollection
 import org.bson.codecs.configuration.CodecRegistries.fromProviders
 import org.bson.codecs.configuration.CodecRegistries.fromRegistries
 import org.bson.codecs.pojo.PojoCodecProvider
@@ -18,10 +20,11 @@ class GeoMain : BukkitPlugin() {
 
         try {
             val codec = fromRegistries(
-                    MongoClientSettings.getDefaultCodecRegistry(),
-                    fromProviders(PojoCodecProvider.builder().automatic(true).build())
+                MongoClientSettings.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder().automatic(true).build())
             )
-            database = MongoClients.create(MongoClientSettings.builder().codecRegistry(codec).build()).getDatabase("test")
+            database =
+                MongoClients.create(MongoClientSettings.builder().codecRegistry(codec).build()).getDatabase("test")
             canCollection = database.getCollection("zone", Can::class.java)
             info("&a連接MongoDB數據庫成功")
 
@@ -39,6 +42,19 @@ class GeoMain : BukkitPlugin() {
     }
 
     override fun onLoad() {
-        info("add...")
+        val logo = """
+                ___           ___           ___     
+               /\__\         /\__\         /\  \    
+              /:/ _/_       /:/ _/_       /::\  \   
+             /:/ /\  \     /:/ /\__\     /:/\:\  \  
+            /:/ /::\  \   /:/ /:/ _/_   /:/  \:\  \ 
+           /:/__\/\:\__\ /:/_/:/ /\__\ /:/__/ \:\__\
+           \:\  \ /:/  / \:\/:/ /:/  / \:\  \ /:/  /
+            \:\  /:/  /   \::/_/:/  /   \:\  /:/  / 
+             \:\/:/  /     \:\/:/  /     \:\/:/  /  
+              \::/  /       \::/  /       \::/  /   
+               \/__/         \/__/         \/__/
+        """
+        info("&5$logo")
     }
 }
